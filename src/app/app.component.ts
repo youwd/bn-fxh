850e9b96b10f29bab923917ac6a996d4c2d39c3b
ywade <youwdhs@126.com>
2019-07-24 20:33:34 +0800

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Lottery';
+  public lucky = '幸运儿';  // 页面显示结果
+  public isStart = true;   // 控制开始 结束按钮显示
+
+  private list = ['叶孟', '钟鑫火', '温宇强', '叶孟2', '钟鑫火2', '温宇强2', '叶孟3', '钟鑫火3', '温宇强3'];
+  private length = this.list.length;
+  private time: any;  // 定时器
+
+  /**
+   * 开始抽奖
+   */
+  public start() {
+    this.isStart = false;
+    this.time = setInterval(() => {
+      const index = Math.floor(Math.random() * this.length);
+      this.lucky = this.list[index];
+    }, 100);
+  }
+
+  /**
+   * 结束
+   */
+  public stop() {
+    clearInterval(this.time);
+    this.isStart = true;
+  }
+
 }
